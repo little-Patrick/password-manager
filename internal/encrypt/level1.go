dcb2c305c099e38b76412236309854037ceefca9
little-Patrick <Little.patrick@outlook.com>
2025-03-23 18:57:58 -0600

    CHANGE: take out password shift to simplify for now

--- internal/encrypt/level1.go
+++ internal/encrypt/level1.go
@@ -13,9 +13,10 @@ import (
 type Level1 struct {
 	password string
 	key1     string
+	key2 	 string
 }
 
-func NewLevel1(password string, key1 string) *Level1 {
+func NewLevel1(password string, key1 string, key2 string) *Level1 {
 	if password == "" {
 		// TODO: Handle error properly (e.g., return an error instead)
 		panic("Password cannot be empty")
@@ -23,50 +24,29 @@ func NewLevel1(password string, key1 string) *Level1 {
 	if key1 == "" {
 		key1 = fmt.Sprintf("%d", rand.Intn(90000)+1000)
 	}
-	return &Level1{password: password, key1: key1}
+	if key2 == "" {
+		key2 = fmt.Sprintf("%d", rand.Intn(90000)+1000)
+	}
+	return &Level1{password: password, key1: key1, key2: key2}
 }
 
-func (e *Level1) Encrypt() string {
+func (e *Level1) Encrypt() (string, error) {
 	return e.passwordShift(true)
 }
-	// Pin Key
-	// square key
-	// LATER: Divide key to go down different paths
-	// take last 5 digits of sqyared key
-	// A key: first two digits (02)
-	// B key: second and third digits (27)
-	// C key: third and fourth digits (71)
-	// D key: fourth and fifth digits (15)
 
-	// password key
-	// multiply the ord of each letter together
-	// take the last four digits and make a map 
-	//
-
-func (e *Level1) Decrypt() string {
+func (e *Level1) Decrypt() (string, error) {
 	return e.passwordShift(false)
 }
 
-func (e *Level1) passwordShift(isEncrypt bool) string {
+func (e *Level1) passwordShift(isEncrypt bool) (string, error) {
 	if isEncrypt {
-		e.finalKey()
+		key, err := e.keyMap()
+		if err != nil {
+			return "", err
+		}
 	} else {
 	}
-	return ""
-}
-
-func (e *Level1) finalKey() map[string]int  {
-	passwordKey, _ := e.passwordKeyMap()
-	pinKey, _ := e.keyMap()
-
-	finalKey := map[string]int {
-		"A": pinKey["A"] + passwordKey["A"],
-		"B": pinKey["B"] / passwordKey["B"],
-		"C": pinKey["C"] * passwordKey["C"],
-		"D": pinKey["D"] - passwordKey["D"],
-	}
-
-	return finalKey
+	return "", nil
 }
 
 func (e *Level1) keyMap() (map[string]int, error) {
@@ -91,38 +71,6 @@ func (e *Level1) keyMap() (map[string]int, error) {
 	}
 	runtime.Breakpoint()
 	
-	return intKeyMap, nil
-}
-
-func (e *Level1) passwordKeyMap() (map[string]int, error) {
-	splitPassword := strings.Split(e.password, "")
-	multiplyKey := 1.0
-	for i := 0; i < len(splitPassword); i++ {
-		if len(splitPassword[i]) > 0 {
-			char := int([]rune(splitPassword[i])[0]) 
-			key := float64(char)
-			multiplyKey *= key
-		}
-	}
-	square := math.Pow(multiplyKey, 2) 
-	keyToS := strconv.FormatFloat(square, 'f', -1, 64)
-	splitKey := strings.Split(keyToS, "")
-	// TODO: Make key different from keyMap
-	mapKey := map[string]string {
-		"A": splitKey[0] + splitKey[1],
-		"B": splitKey[1] + splitKey[2],
-		"C": splitKey[2] + splitKey[3],
-		"D": splitKey[3] + splitKey[4],
-	}
-	// TODO: Make helper function ID: 1
-	intKeyMap := make(map[string]int)
-	for key, value := range mapKey {
-		intValue, err := strconv.Atoi(value)
-		if err != nil {
-			return nil, fmt.Errorf("Something went wrong %s: %v", key, err)
-		}
-		intKeyMap[key] = intValue
-	}
 	return intKeyMap, nil
 }
 
